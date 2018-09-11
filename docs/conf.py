0f6200b9d1475222ea30c49ddfd8ced7bc8c4886
Rich Bennett <rb2745@att.com>
2018-09-11 07:38:05 -0400

    Switch theme and add report issue
    Switch to sphinx_rtd_theme
    Add report issue in Jira with current page/release context
    
    Change-Id: Ie237ee4d084f2b8bd025acc7499d19563f6e3e68
    Issue-ID: DOC-310
    Signed-off-by: Rich Bennett <rb2745@att.com>

--- docs/conf.py
+++ docs/conf.py
@@ -131,8 +131,8 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'bootstrap'
-html_theme = 'classic'
+#html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -488,3 +488,6 @@ needs_template_collapse = """
         | parents: :need_outgoing:`{{id}}`
 {% endif -%}
 """
+
+def setup(app):
+    app.add_stylesheet("css/ribbon.css")
