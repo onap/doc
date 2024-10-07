e3c2e68decb9d1dcdbec16aa0323f39ad4c133b7
thmsdt <thomas.kulik@telekom.de>
2024-10-07 16:08:57 +0200

    update for interactive architecture diagram
    Issue-ID: DOC-826
    
    Change-Id: I62164aa4558b1b21dd8102a9c10f83dd7c37799e
    Signed-off-by: thmsdt <thomas.kulik@telekom.de>

--- docs/conf.py
+++ docs/conf.py
@@ -120,5 +120,7 @@ def setup(app):
     app.add_css_file("css/ribbon.css")
 
 linkcheck_ignore = [
-    r'http://localhost:\d+/'
+    r'http://localhost:\d+/',
+    r'http://.*',
+    r'https://.*'
 ]
